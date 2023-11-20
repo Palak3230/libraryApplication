7e682d667bc9de5d951301cde96792e688042aab
Palak3230 <91584302+Palak3230@users.noreply.github.com>
2023-11-20 16:44:25 +0530

    Update BookController.java

--- src/main/java/com/nagarro/libraryApplication/controller/BookController.java
+++ src/main/java/com/nagarro/libraryApplication/controller/BookController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<List<Book>> getAllBooks(){
         return ResponseEntity.ok(bookService.getAllBooks());
     }
